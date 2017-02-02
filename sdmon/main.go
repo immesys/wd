377079092e8a7a77887bcb888262b841f284dcff
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-02-02 14:20:35 -0800

    Revert bad parts of "bump ver"
    This reverts commit 0bb86af7a2e5585738cf658884c5a65aa70c2a73.

--- sdmon/main.go
+++ sdmon/main.go
@@ -17,8 +17,11 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sdmon"
 	app.Usage = "Maintain systemd watchdogs"
-	app.Version = "1.4.0"
+	app.Version = "1.3.0"
 	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "prefix",
+		},
 		cli.DurationFlag{
 			Name:  "interval",
 			Value: 2 * time.Minute,
@@ -120,10 +123,14 @@ func runApp(c *cli.Context) error {
 		displayName: make(map[string]string),
 		lastAction:  make(map[string]Action),
 	}
-	e.prefix = os.Getenv("POP_ID")
-	e.prefix = strings.Replace(e.prefix, "-", "_", -1)
-	e.prefix = strings.ToLower(e.prefix)
-	e.prefix = "410.br." + e.prefix + "."
+	e.prefix = c.String("prefix")
+	if e.prefix == "" {
+		fmt.Println("You need to specify --prefix")
+		os.Exit(1)
+	}
+	if !strings.HasSuffix(e.prefix, ".") {
+		e.prefix += "."
+	}
 	e.interval = c.Duration("interval")
 	e.timeout = int((c.Duration("interval") / time.Second)) * 2
 	e.healthyInterval = c.Duration("holdoff")
@@ -140,8 +147,13 @@ func runApp(c *cli.Context) error {
 	if err != nil {
 		panic(err)
 	}
+	err = e.conn.Subscribe()
+	if err != nil {
+		panic(err)
+	}
+
 	for {
 		e.Scan()
-		time.Sleep(e.interval)
+		time.Sleep(2 * time.Second)
 	}
 }
